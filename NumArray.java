fff9f78f120104fb2015419b8eb03c686bfe75cc
kyleyiyang <kyleyiyang@gmail.com>
2021-06-14 21:05:03 -0400

    Update NumArray.java

--- NumArray.java
+++ NumArray.java
@@ -43,3 +43,25 @@ class NumArray {
  * obj.update(index,val);
  * int param_2 = obj.sumRange(left,right);
  */
+
+
+*******************************************
+class NumArray {
+    int[] sum;
+    public NumArray(int[] nums) {
+        sum=new int[nums.length+1];
+        for (int i=0;i<nums.length;i++) {
+            sum[i+1]=sum[i]+nums[i];
+        }
+    }
+    
+    public int sumRange(int left, int right) {
+        return sum[right+1]-sum[left];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
